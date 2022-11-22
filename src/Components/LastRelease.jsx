328452bdc4770844b39d470d55c2e458986ce4d0
Francisco-Fortes <fortesfco@gmail.com>
2022-11-22 21:16:41 +0100

    App working

--- src/Components/LastRelease.jsx
+++ src/Components/LastRelease.jsx
@@ -1,22 +1,20 @@
-import {Container, Row, Card, Button } from "react-bootstrap";
-import scifiBook from "../data/books/scifi.json";
+import { Container, Row, Card, Button } from "react-bootstrap";
+import scifiBook from "../Data/books/scifi.json";
 
 const LastRelease = () => {
   return (
     <Container>
       <Row>
-            {scifiBook.map(book) => (
-        <Card key={book.asin} style={{ width: "18rem" }}>
-             <Card.Img variant="top" src={book.img} alt={book.title}/>
-             <Card.Body>
-               <Card.Title>{book.title}</Card.Title>
-               <Card.Text>
-                 {book.category}
-               </Card.Text>
-               <Button variant="primary">4FREE</Button>
-             </Card.Body>   
-        </Card>
-            )}
+        {scifiBook.map((book) => (
+          <Card key={book.asin} style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={book.img} alt={book.title} />
+            <Card.Body>
+              <Card.Title>{book.title}</Card.Title>
+              <Card.Text>{book.category}</Card.Text>
+              <Button variant="primary">4FREE</Button>
+            </Card.Body>
+          </Card>
+        ))}
       </Row>
     </Container>
   );
