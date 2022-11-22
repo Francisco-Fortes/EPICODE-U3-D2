196a4bda50872219d2503799362bd21808a10bdd
Francisco-Fortes <fortesfco@gmail.com>
2022-11-22 16:20:42 +0100

    Footer component created

---
+++ src/Components/MyFooter.jsx
@@ -0,0 +1,13 @@
+const MyFooter = () => {
+  return (
+    <footer>
+      <container>
+        <row className="justify-content-around">
+          <span>Author: Francisco Fortes</span>
+          <span>Running out of time @16:20</span>
+        </row>
+      </container>
+    </footer>
+  );
+};
+export default MyFooter;
